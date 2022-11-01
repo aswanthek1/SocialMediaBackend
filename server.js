3a14f92c5b1680e680d067fcb1c95d764f03f1e1
Aswanth Raveendran E K <aswanthek1@gmail.com>
2022-11-01 23:46:19 +0530

    Database cluster created

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middlewares/errorMiddleware')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const port =process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server started at port ${port}`))
